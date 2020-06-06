7423dbca0c59b8d2e4685f6f992112668c961be3
dingobar <mmorset@gmail.com>
2020-06-06 20:14:58 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace eleventh
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new MetAPIRequester();
+
+            var wf = await client.GetForecastAsync();
+
+            var forecasts = wf.product.time;
+
+            var precipitationforecasts = forecasts.OrderBy(x => x.from).Where(x => x.location.precipitation != null && x.ForecastLength == 1).ToList();
+
+            foreach(var forecast in precipitationforecasts){
+                Console.WriteLine($"");
+            }
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
---
+++ controllers/WeatherForecast.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.WebUtilities;
+
+
+namespace eleventh
+{
+
+    class MetAPIRequester
+    {
+        private HttpClient client = new HttpClient();
+        private double lat = 55.696068;
+        private double lon = 12.546126;
+        private double msl = 10;
+        private string metURL = "https://api.met.no/weatherapi/locationforecast/1.9/.json";
+
+        public async Task<WeatherForecast> GetForecastAsync()
+        {
+            var parametersToAdd = new System.Collections.Generic.Dictionary<string, string> {
+                { "lat", lat.ToString() },
+                { "lon", lon.ToString() },
+                { "msl", msl.ToString() }
+                };
+            var requestUri = QueryHelpers.AddQueryString(metURL, parametersToAdd);
+            Console.WriteLine(requestUri.ToString());
+            var result = await client.GetAsync(requestUri);
+            Console.WriteLine(result.StatusCode);
+            var resultString = await result.Content.ReadAsStringAsync();
+
+            var weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(resultString);
+
+            return weatherForecast;
+        }
+    }
+}
